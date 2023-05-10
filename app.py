1b75af9b97ff5d19b77e45a16fe6d28cad47754a
Darsh <57062224+patel-16@users.noreply.github.com>
2023-05-11 02:24:33 +0530

    Added 3rd model for semantinc seg

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from PIL import Image
-from clf import predict_with_resnet101, segment_with_mrcnn
+from clf import predict_with_resnet101, segment_with_mrcnn, sem_segment
 import os
 import requests
+import gdown
 
 
-app_mode = st.sidebar.selectbox('Select Page',['Model_1', 'Model_2'])
+    
+app_mode = st.sidebar.selectbox('Select Page',['Model_1', 'Model_2', 'Model_3'])
 
 if app_mode=='Model_1':
 
@@ -41,7 +43,34 @@ if app_mode=='Model_2':
         st.write("Just a second...")
         img, _, __ = segment_with_mrcnn(file_up)
 
+        print(img.shape)
         st.image(img, caption='Segmented Image.', use_column_width=True)
+        
+        # print out the top 5 prediction labels with scores
+        # for i in labels:
+        #     st.write("Prediction (index, name)", i[0], ",   Score: ", i[1])
+
+if app_mode=='Model_3':
+
+    file_up = st.file_uploader("Upload an image", type="jpg")
+
+    # image = Image.open(file_up)
+    # st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+    if file_up is not None:
+        image = Image.open(file_up)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
+        st.write("Just a second...")
+        img = sem_segment(file_up)
+        print(img.shape)
+
+
+        loaded_img = Image.open('p.jpg')
+
+        st.image(loaded_img,  caption='Segmented Image.', use_column_width=True)
+        
+        # st.image(img, caption='Segmented Image.', use_column_width=True)
         
         # print out the top 5 prediction labels with scores
         # for i in labels:
