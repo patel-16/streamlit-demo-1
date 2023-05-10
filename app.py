7d06f696a6ba1ce35633a6cfb80cc6d9001a3e1a
Darsh <57062224+patel-16@users.noreply.github.com>
2023-05-10 22:04:08 +0530

    Removed the download function from server
    Was a big mistake since server file will always keep checking constantly

--- app.py
+++ app.py
@@ -5,20 +5,6 @@ import os
 import requests
 
 
-if os.path.isfile('resnet_scripted.pt')==False:
-
-
-    URL_RESNET = "https://drive.google.com/file/d/18Rh9mN8FXb-8gsq9KsC9pxE3boqLrpFH/view?usp=sharing"
-    response_res = requests.get(URL_RESNET)
-    open("resnet_scripted.pt", "wb").write(response_res.content)
-
-if os.path.isfile('mrcnn_scripted.pt')==False:
-
-    URL_MRCNN = "https://drive.google.com/file/d/1E86dc0S3gx8P0Prtm1yqNVG5OuDnOtqX/view?usp=sharing"
-    response_mrcnn = requests.get(URL_MRCNN)
-    open("mrcnn_scripted.pt", "wb").write(response_mrcnn.content)
-
-
 app_mode = st.sidebar.selectbox('Select Page',['Model_1', 'Model_2'])
 
 if app_mode=='Model_1':
